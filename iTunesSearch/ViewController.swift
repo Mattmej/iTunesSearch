e0446f7c7af634c8f76dac1fa6c7be03d17edb2e
Matt Mejia <matt@ADMINs-MacBook-Pro.local>
2018-12-13 00:13:39 -0500

    Add podfile and Alamofire to project.

--- iTunesSearch/ViewController.swift
+++ iTunesSearch/ViewController.swift
@@ -7,8 +7,12 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
+    
+    // We will change this later. Need to make limit changeable.
+    let MAIN_URL = "http://itunes.apple.com/us/rss/topalbums/limit=10/json"
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +22,17 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    
+    
+    //MARK: - Networking
+    /**************************************************/
+    
+    func getAlbumData(url: String, parameters:[String:String]) {
+        
     }
 
 
